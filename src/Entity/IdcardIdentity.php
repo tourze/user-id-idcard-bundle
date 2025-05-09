3949575e00c4ef3676dce6eb148075ea49aee0d2
github-ci <you@example.com>
2025-05-09 10:58:57 +0000

    refactor(entity): 添加统一的 getAccounts 方法

--- src/Entity/IdcardIdentity.php
+++ src/Entity/IdcardIdentity.php
@@ -155,4 +155,9 @@ class IdcardIdentity implements IdentityInterface
             'updateTime' => $this->getUpdateTime()?->format('Y-m-d H:i:s'),
         ]);
     }
+
+    public function getAccounts(): array
+    {
+        return [];
+    }
 }
